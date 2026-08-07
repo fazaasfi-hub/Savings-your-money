c7645ffc707ee91ac132d14d13ed4e1d7f225e83
fazaasfi-hub <fazaasfi@gmail.com>
2026-08-07 19:26:16 +0700

    refactor: migrate to capacitor BridgeActivity
    Transition the main activity to extend BridgeActivity to support capacitor-based web-to-native integration, removing native Jetpack Compose UI dependencies.

--- android/app/src/main/java/com/fzsavings/app/MainActivity.kt
+++ android/app/src/main/java/com/fzsavings/app/MainActivity.kt
@@ -1,42 +1,6 @@
 package com.fzsavings.app
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import androidx.lifecycle.ViewModelProvider
-import com.fzsavings.app.data.local.SavingsDatabase
-import com.fzsavings.app.data.repository.SavingsRepositoryImpl
-import com.fzsavings.app.presentation.viewmodel.SavingsViewModel
-import com.fzsavings.app.ui.navigation.AppNavigation
-import com.fzsavings.app.ui.theme.FZSavingsTheme
+import com.getcapacitor.BridgeActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : BridgeActivity()
 
-        // Initialize Local Storage Database
-        val database = SavingsDatabase.getInstance(applicationContext)
-        val repository = SavingsRepositoryImpl(database.dao())
-        val viewModel = ViewModelProvider(this, SavingsViewModel.Factory(repository))[SavingsViewModel::class.java]
-
-        setContent {
-            FZSavingsTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    AppNavigation(
-                        navController = navController,
-                        viewModel = viewModel
-                    )
-                }
-            }
-        }
-    }
-}
